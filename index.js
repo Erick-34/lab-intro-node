6d2bdc9cb274181e0355f83281acfbd1b45c168f
Erick-34 <erickoliveira2060@gmail.com>
2020-06-26 15:08:16 -0300

    Done

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    if (SortedList == []) {
+      throw new Error("OutOfBounds");
+    } else {
+      this.items.push(item);
+      this.length++;
+      return this.items.sort((a, b) => a - b), this.length;
+    }
+  }
+  get(pos) {
+    if (this.items.indexOf(pos) >= 0) {
+      return this.items.indexOf(pos);
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[this.length - 1];
+  }
 
-  add(item) {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.min(...this.items);
+  } 
 
-  get(pos) {}
+  sum() {
+    return this.items.reduce((acc, cv) => acc + cv, 0);
+  }
 
-  max() {}
-
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
